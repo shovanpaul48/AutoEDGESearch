4eaeecb2a680bdecb33a312961d72d0c22ba61bb
Shovan Sundar Paul <shovanpaul48@gmail.com>
2023-05-19 22:17:16 +0530

    modified

--- AutoEDGEsearch.py
+++ AutoEDGEsearch.py
@@ -10,28 +10,24 @@ import time
 import numba
 import psutil
 
+import random, string
+
+def randomword(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+# testing variable 
+''' 
 RandomText30 = "abcdefghijklmnopqrstuvwxyz123456"
-RandomText10 = "uhbueehjwhgwr"
+RandomText10 = "uhbueehjdeaw"
 ProfileIDs = ["Default","1","2","3","4","5","7","8","9","10","11","12","13","14","15"]
+'''
+
 
 
 
 def close_edge_browser():
     subprocess.run(["taskkill", "/im", "msedge.exe", "/f"])
-
-
-
-def terminate_edge_tabs(profile_name):
-    browser_processes = ["msedge.exe"]
-
-    for proc in psutil.process_iter():
-        try:
-            process_name = proc.name()
-            cmdline = proc.cmdline()
-            if process_name.lower() in browser_processes and any(profile_name.lower() in arg.lower() for arg in cmdline):
-                proc.kill()
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ProcessLookupError):
-            pass
 
 
 
@@ -44,26 +40,42 @@ def run_command(command):
         print(f"Error executing command: {e}")
 
 # @jit(nopython=True)
-def main():
-    # start = time.now
-    # for each profile it will search the string 
-    for i in range(len(ProfileIDs)):
-        profile = ProfileIDs[i]
+def main(profileNo):
+
+    for i in range(0,profileNo):
         if i != 0 :
-            profile = "Profile " + ProfileIDs[i]
-        
+            profile = "Profile " + str(i)
+        else : 
+            profile = "Default "
         print(profile)
-            
-        for j in range(len(RandomText30)-1,-1,-1):
-            query = RandomText30[0:j]
+
+        searchText = randomword(32)
+
+        for j in range(len(searchText)-1,-1,-1):
+            query = searchText[0:j]
             print(j,"   ", query)
 
             command = f'start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" https://www.bing.com/search?q={query}  --profile-directory="{profile}"'  
             run_command(command)
-            time.sleep(1)
+            time.sleep(1.5)
             if (j % 7 == 0):
                 close_edge_browser()
+            time.sleep(2)
+
+
+def clean_edge_cache():
+    try:
+        # Execute the command to clear Edge cache
+        subprocess.run(['cmd', '/c', 'start', 'msedge', '--clear-achievements'], check=True)
+        print("Edge cache cleaned successfully.")
+    except subprocess.CalledProcessError as e:
+        print("Error occurred while cleaning Edge cache:", e)
 
 
 if __name__ == "__main__":
-    main()
+    profileNo = int(input("Number of profiles you have (include Default) : "))
+    # skipProfile = input("If search is complete for any profile enter those profile no (coma separated or enter NA ) : ")
+    main(profileNo)
+
+    # Call the function to clean the Edge cache
+    clean_edge_cache()
